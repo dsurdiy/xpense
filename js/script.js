0904375bcc5e520343d01f0ff8edf86078286594
dsurdiy <dsurdiy@ukr.net>
2025-02-07 21:21:45 +0200

    animations added

--- js/script.js
+++ js/script.js
@@ -63,17 +63,17 @@ if (document.querySelector('.testimonials__swiper')) {
 
 // -------------- Animations (IntersectionObserver) -------------------
 
-// const items = document.querySelectorAll("[data-watch]");
-// const options = {
-//    threshold: 0.2,
-// }
-// const callback = (entries) => {
-//    entries.forEach(entry => {
-//       entry.isIntersecting ? entry.target.classList.add("active") : entry.target.classList.remove("active");
-//    });
-// }
-// const observer = new IntersectionObserver(callback, options);
+const items = document.querySelectorAll("[data-watch]");
+const options = {
+   threshold: 0.2,
+}
+const callback = (entries) => {
+   entries.forEach(entry => {
+      entry.isIntersecting ? entry.target.classList.add("active") : entry.target.classList.remove("active");
+   });
+}
+const observer = new IntersectionObserver(callback, options);
 
-// items.forEach(item => {
-//    observer.observe(item);
-// });
+items.forEach(item => {
+   observer.observe(item);
+});
